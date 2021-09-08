8711ed8097ab9f6575642910e0b34be2037dcd41
LuisaMunoz00 <valencia61045@gmail.com>
2021-09-07 21:04:25 -0500

    Modificación script Python

--- project/app/scr/server.py
+++ project/app/scr/server.py
@@ -1,5 +1,17 @@
 from random import *
+import string, os
 
-a = randint(11,19)
+nombres_archivos = []
 
-print(a)
+numero_archivos = randint(10,20)
+longitud_nombre = 8
+for x in range(numero_archivos):
+   nombre_archivo = ".join(choice(string.ascii_letters + string.digits) for _ in range(logintud_nombre))
+   nombres_archivos.append(nombre_archivo)
+
+print(nombres_archivos)
+print("numero de archivos creados:", len (nombres_archivos))
+
+for i in nombres_archivos:
+   nombre = str(i) + ".txt"
+   txt = open(
