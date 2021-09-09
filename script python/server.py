31e958f05755d26a221ec2d9be543c755cc77606
LuisaMunoz00 <valencia61045@gmail.com>
2021-09-08 21:39:23 -0500

    Se actualiza el script

--- script python/server.py
+++ script python/server.py
@@ -1,12 +1,11 @@
-from random import *
-import string, os
+import random, string, os
 
 nombres_archivos = []
 
-numero_archivos = randint(10,20)
+numero_archivos = random.randint(10,20)
 longitud_nombre = 8
 for x in range(numero_archivos):
-   nombre_archivo = ".join(choice(string.ascii_letters + string.digits) for _ in range(logintud_nombre))
+   nombre_archivo = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(longitud_nombre))
    nombres_archivos.append(nombre_archivo)
 
 print(nombres_archivos)
@@ -14,4 +13,8 @@ print("numero de archivos creados:", len (nombres_archivos))
 
 for i in nombres_archivos:
    nombre = str(i) + ".txt"
-   txt = open(
+   txt = open(r"D:/" + nombre, 'w')
+   numero_caracteres = random.randint(1000,5000)
+   escritura = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(numero_caracteres))
+   txt.write(escritura)
+   txt.close
